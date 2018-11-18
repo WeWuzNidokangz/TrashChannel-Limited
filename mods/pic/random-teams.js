4704bcf427fc4c3302aba1b6e9b742152773826e
We Wuz Nidokangz <globalistgamer@gmail.com>
2018-11-19 00:15:54 +0900

    Added PiC Hackmons Cup

---
+++ mods/pic/random-teams.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const RandomTeams = require('./../../data/random-teams');
+
+class RandomPiCTeams extends RandomTeams {
+	randomHCPiCTeam() {
+		let team = this.randomHCTeam();
+		return team;
+	}
+}
+
+module.exports = RandomPiCTeams;
